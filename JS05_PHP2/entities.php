e28ccd2ad2be067bdf07ffeeff729f813ed39d08
Binaaa04 <sabrinarahmadini60@gmail.com>
2024-09-24 19:51:08 +0700

    JS 05 - Practicum 9 (HTML entities)

---
+++ JS05_PHP2/entities.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>>
+<html>
+<head>
+<title>Entities</title>
+</head>
+<body>
+<p>It&#39; time to read a HTML5 book.</p>
+<p>&nbsp;&nbsp;&nbsp;&nbsp;Advantages of using named entities:
+An entity name is easy to remember. Disadvantages of using named entities: Browsers may not support all entity names, but support for numbers is better.</p>
+<p>&#169; 2023 jti.com</p>
+</body>
+</html>
