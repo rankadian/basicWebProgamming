fb8724c92a0d4dbee1eec0706e693ad1d4f17614
Binaaa04 <sabrinarahmadini60@gmail.com>
2024-09-24 11:29:25 +0700

    Praticum 7 - Escape character

---
+++ JS05_PHP2/string2.php
@@ -0,0 +1,12 @@
+<?php
+echo "New\nline <br>"; //soal 10.a
+echo "New\nline <br>"; //soal 10.a
+echo "Hello\nworld <br>"; //soal 10.c
+echo "Hello\nworld <br>"; //soal 10.d
+
+echo "<pre> Hello\tworld </pre>"; //soal 10.e
+echo "<pre> Hello\tworld </pre>"; //soal 10.f
+
+echo "Say \" no to drug!\" <br>"; //soal 10.g
+echo "Say \" no to drug!\" <br>"; //soal 10.h
+?>
