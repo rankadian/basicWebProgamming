b3f13542fb717e0d8b9c0b0fbf809075023bfb53
Binaaa04 <sabrinarahmadini60@gmail.com>
2024-09-24 19:21:21 +0700

    JS 5 - practicum 7 reverse a string word by word

--- JS05_PHP2/string3.php
+++ JS05_PHP2/string3.php
@@ -1,4 +1,10 @@
 <?php
 $message = "kowe arek malang";
-echo strrev($message)."<br>";
+# ubah variabel menjadi array dengan perintah explode
+$messageperWord = explode(" " ,$message) ;
+# ubah setiap kata datam array menjadi kebalikannya
+$messageperWord = array_map(fn($message) => strrev($message), $messageperWord) ;
+# gabungkan kembali array menjadi string
+$message = implode(" ", $messageperWord) ;
+echo "<br>" .$message;
 ?>
