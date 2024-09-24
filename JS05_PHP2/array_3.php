66eb7750a6fe64d59a3ecc3c2f3c456c7fc66439
Binaaa04 <sabrinarahmadini60@gmail.com>
2024-09-24 21:29:11 +0700

    JS 05 - Practicum 3 (Multidimensional Array)

--- JS05_PHP2/array_3.php
+++ JS05_PHP2/array_3.php
@@ -13,30 +13,19 @@
     </tr>
 <?php
 $movie = array(
-    array ("Avengers: Invinity War", 2018, 8.7),
-    array ("Ihe Avengers", 2012,8.1),
-    array ("Guardians of the Galaxy", 2014,8.1),
-    array ("Iron Man", 2008, 7.9));
-echo "<tr>";
-    echo "<td>". $movie[0] [0]."</td>";
-    echo "<td>". $movie[0] [1]."</td>";
-    echo "<td>". $movie[0] [2]."</td>";
-echo "<tr>";
-echo "<tr>";
-    echo "<td>". $movie[1] [0]."</td>";
-    echo "<td>". $movie[1] [1]."</td>";
-    echo "<td>". $movie[1] [2]."</td>";
-echo "<tr>";
-echo "<tr>";
-    echo "<td>". $movie[2] [0]."</td>";
-    echo "<td>". $movie[2] [1]."</td>";
-    echo "<td>". $movie[2] [2]."</td>";
-echo "<tr>";
-echo "<tr>";
-    echo "<td>". $movie[3] [0]."</td>";
-    echo "<td>". $movie[3] [1]."</td>";
-    echo "<td>". $movie[3] [2]."</td>";
-echo "<tr>";  
+    array ("Avengers: Infinity War", 2018, 8.7),
+    array ("The Avengers", 2012, 8.1),
+    array ("Guardians of the Galaxy", 2014, 8.1),
+    array ("Iron Man", 2008, 7.9)
+);
+
+for ($i = 0; $i < count($movie); $i++) {
+    echo "<tr>";
+    echo "<td>". $movie[$i][0] ."</td>";
+    echo "<td>". $movie[$i][1] ."</td>";
+    echo "<td>". $movie[$i][2] ."</td>";
+    echo "</tr>";
+}
 ?>
 </table>
 </body> 
