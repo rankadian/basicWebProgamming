dca5531424c5a608576f9d29bfd58fbd2386b4e1
Binaaa04 <sabrinarahmadini60@gmail.com>
2024-09-24 10:23:52 +0700

    Praticum 4 - Calling a Function Inside Another Function

--- JS05_PHP2/function_return.php
+++ JS05_PHP2/function_return.php
@@ -4,5 +4,11 @@ function ageCount($birthDate, $currentYear){
 $age = $currentYear - $birthDate;
 return $age;
 }
-echo "my age now is " .ageCount(2004, 2024) ." year";
+function introduce($name, $salam = "Assalamualaikum"){
+    echo $salam. ",<br>";
+    echo "introduce my name is ".$name."<br>";
+    echo "my age now is " .ageCount(2004, 2024) ." year<br>";
+    echo "Nice to meet you <br>";
+}
+introduce("Bina");
 ?>
