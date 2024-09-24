5e9153ded66198258a63158b621b8581386d6cd1
Binaaa04 <sabrinarahmadini60@gmail.com>
2024-09-24 09:25:13 +0700

    Praticum2

---
+++ JS05_PHP2/array_2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf—8">
+<meta name = "viewport" content="width=device—width, initial-scale=1">
+<title></title>
+</head>
+<body>
+    <?php
+    $lecture = [
+    'name' => 'Elok Nur Hamdana' ,
+    'domicile' => 'Malang' ,
+    'gender' => 'Woman'];
+        echo "Name : {$lecture['name']}<br>";
+        echo "Domicile : {$lecture['domicile']}<br>";
+        echo "Gender : {$lecture['gender']}<br>";
+    ?> 
+</body>
+</html>
