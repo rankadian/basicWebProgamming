f5306e2bb40a8b223e1b67ab5c3c89836963e8fe
Binaaa04 <sabrinarahmadini60@gmail.com>
2024-09-24 09:34:29 +0700

    Praticum 1

---
+++ JS05_PHP2/array_1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h2>Indexed Array</h2>
+<?php
+$LecturerList=["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"] ;
+echo $LecturerList [2]."<br>";
+echo $LecturerList [0]."<br>";
+echo $LecturerList [1]."<br>";
+?>
+</body>
+</html>
