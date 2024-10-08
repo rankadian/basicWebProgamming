aacad45a75af72517253062eeda3ceee0e3c39ce
Binaaa04 <sabrinarahmadini60@gmail.com>
2024-10-08 13:15:28 +0700

    JS07_Practicum 6_Advanced Form_Step 2

---
+++ JS07_PHP&jQuery/form_next.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>This Example Form w/ PHP</title>
+    </head>
+    <body>
+        <h2>Example Form</h2>
+        <form method="POST" action="form_next.php">
+            <lable for="fruit">Choose fruit : </lable>
+            <select name="fruit" id="fruit">
+                <option value="strawberry">Strawberry</option>
+                <option value="mango">Mango</option>
+                <option value="banana">Banana</option>
+                <option value="dragonfruit">Dragon Fruit</option>
+            </select>
+            <br>
+            <lable>Choose ur favorite color : </lable>
+            <br>
+            <input type="checkbox" name="color[]" value="red">Nude<br>
+            <input type="checkbox" name="color[]" value="blue">Blue<br>
+            <input type="checkbox" name="color[]" value="green">Pink<br>
+            <br>
+            <lable>Choose ur gender : </lable>
+            <br>
+            <input type="radio" name="gender" value="boy">Boy<br>
+            <input type="radio" name="gender" value="girl">Girl<br>
+            <br>
+            <input type="submit" value="submit">
+        </form>
+    </body>
+</html>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $selectedFruit = $_POST['fruit'];
+    if (isset($_POST['color'])) {
+        $selectedColor = $_POST['color'];
+    } else {
+        $selectedColor = [];
+    }
+    $selectedGender = $_POST['gender'];
+    echo "The fruit u choose is : ". $selectedFruit . "<br>";
+    if (!empty($selectedColor)) {
+         echo "ur favorite color is : ".implode(", ",$selectedColor). "<br>";
+    } else {
+        echo "u dont choose ur favorite color <br>";  
+    }
+    echo "ur gender is : " . $selectedGender;
+}
+?>
