c1bfaea001d1bc7c708956518d3f91f100a4ba38
Binaaa04 <sabrinarahmadini60@gmail.com>
2024-10-08 13:16:29 +0700

    JS07_Practicum 6_Advanced Form_Step 5

---
+++ JS07_PHP&jQuery/form_ajax.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>This Example Form w/ PHP</title>
+    </head>
+    <body>
+        <h2>Example Form</h2>
+        <form method="POST" action="form_next.php">
+            <lable for="fruit">Choose fruit : </lable>
+            <select name="fruit" id="fruit">
+                <option value="apple">Apple</option>
+                <option value="mango">Mango</option>
+                <option value="banana">Banana</option>
+                <option value="orange">Orange</option>
+            </select>
+            <br>
+            <lable>Choose ur favorite color : </lable>
+            <br>
+            <input type="checkbox" name="color[]" value="red">Red<br>
+            <input type="checkbox" name="color[]" value="blue">Blue<br>
+            <input type="checkbox" name="color[]" value="green">Green<br>
+            <br>
+            <lable>Choose ur gender : </lable>
+            <br>
+            <input type="radio" name="gender" value="boy">Boy<br>
+            <input type="radio" name="gender" value="girl">Girl<br>
+            <br>
+            <input type="submit" value="submit">
+        </form>
+        <div id="result">
+
+        </div>
+        <script>
+            $(document).ready(function(){
+                $("#myForm").submit(
+                    function(e){
+                        e.preventDefault();
+                        var formData = $("#myForm").serialize();
+                        $.ajax({
+                            url:"form_next.php",
+                            type: "POST",
+                            data: formData,
+                            success: function(response){
+                                $("#result").html(response);
+                        }
+                    });
+                });
+            });
+        </script>
+    </body>
+</html>
