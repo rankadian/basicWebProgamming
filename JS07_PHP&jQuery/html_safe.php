8064fe8959abf7577b5ddffe84d73e000cdd6ded
Binaaa04 <sabrinarahmadini60@gmail.com>
2024-10-08 19:28:05 +0700

    JS07_Practicum 7_Form Validation_Assignment 1

--- JS07_PHP&jQuery/html_safe.php
+++ JS07_PHP&jQuery/html_safe.php
@@ -1,3 +1,22 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Basic HTML Injection Protection</title>
+</head>
+<body>
+    <h2>Form Input</h2>
+    <form method="POST" action="">
+        <label for="input">Input ur name in here:</label>
+        <input type="text" id="input" name="input"><br><br>
+
+        <label for="email">Input ur email in here:</label>
+        <input type="text" id="email" name="email"><br><br>
+
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Ambil input dari form
@@ -9,17 +28,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Tampilkan input yang sudah aman
     echo "result input is safe: " . $input;
 }
+if (isset($email) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo "Email is valid: " . $email;
+} else {
+    echo "<BR>Invalid email format!";
+}
+
 ?>
-<!DOCTYPE html>
-<head>
-    <title>Basic HTML Injection Protection</title>
-</head>
-<body>
-    <h2>Form Input</h2>
-    <form method="POST" action="">
-        <label for="input"Input teks:</label>
-        <input type="text" id="input" name="input">
-        <input type="submit" value="Submit">
-    </form>
-</body>
-</html>
+
+
