201f38a61ec3a1beb23d1fa7390f10f8eac7cb1b
Binaaa04 <sabrinarahmadini60@gmail.com>
2024-10-15 09:06:55 +0700

    JS08_Practicum 1_Modify number 10

--- JS08_PHPFormUpload/upload.php
+++ JS08_PHPFormUpload/upload.php
@@ -1,23 +1,18 @@
 <?php
 if (isset($_POST["submit"])) {
     $targetdir = "uploads/";
-    $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
+    $targetfile = $targetdir .basename($_FILES["myfile"]["name"]);
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
-    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
-    $maxsize = 5 * 1024 * 1024;
-    
-    // Check if the file is allowed and its size is within the limit
-    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
-        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
-            echo "File successfully uploaded.<br>";
-
-            // Display the uploaded image as a thumbnail
-            echo "<img src='$targetfile' width='200' style='height:auto;' alt='Uploaded Image'>";
+    $allowedExtensions = array("txt","pdf", "doc", "docx");
+    $maxsize = 3*1024*1024;
+    if (in_array($fileType, $allowedExtensions)&& $_FILES["myfile"]["size"]<=$maxsize) {
+        if (move_uploaded_file($_FILES["myfile"]["tmp_name"],  $targetfile)) {
+            echo "File successed upload";
         } else {
-            echo "Failed to upload file.";
+            echo "Failed to uploaded file";
         }
     } else {
-        echo "File is invalid or exceeds the specified size limit.";
-    }
+        echo "file is invalid or exceeds the specified capacity size";
+} 
 }
 ?>
