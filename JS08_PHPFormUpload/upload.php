23e928799b7f32c6f3b6279f3361cdf6fbc91d70
Binaaa04 <sabrinarahmadini60@gmail.com>
2024-10-15 08:52:16 +0700

    JS08_Practicum 1_Modify number 6

--- JS08_PHPFormUpload/upload.php
+++ JS08_PHPFormUpload/upload.php
@@ -2,11 +2,17 @@
 if (isset($_POST["submit"])) {
     $targetdir = "uploads/";
     $targetfile = $targetdir .basename($_FILES["myfile"]["name"]);
-    if (move_uploaded_file($_FILES["myfile"]["tmp_name"],  $targetfile)) {
-        echo "File successed upload";
+    $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
+    $allowedExtensions = array("jpg","jpeg", "png", "gif");
+    $maxsize = 5*1024*1024;
+    if (in_array($fileType, $allowedExtensions)&& $_FILES["myfile"]["size"]<=$maxsize) {
+        if (move_uploaded_file($_FILES["myfile"]["tmp_name"],  $targetfile)) {
+            echo "File successed upload";
+        } else {
+            echo "Failed to uploaded file";
+        }
     } else {
-        echo "Failed to uploaded file";
-    }
-    
+        echo "file is invalid or exceeds the specified capacity size";
+} 
 }
 ?>
