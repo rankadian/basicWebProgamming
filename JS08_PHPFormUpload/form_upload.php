ba7e813462822fdc8f9e2fbe2629cadf4ec9be95
Binaaa04 <sabrinarahmadini60@gmail.com>
2024-10-15 08:36:54 +0700

    JS08_Practicum 1_Files

---
+++ JS08_PHPFormUpload/form_upload.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <tltle>File Upload</tltle>
+    </head>
+    <body>
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="myfile">
+            <input type="submit" name="submit">
+        </form>
+    </body>
+</html>
