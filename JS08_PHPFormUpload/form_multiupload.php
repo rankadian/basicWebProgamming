d9bf57daa640bd5f5a4e76960a94ad10255ce37e
Binaaa04 <sabrinarahmadini60@gmail.com>
2024-10-15 09:43:56 +0700

    JS08_Practicum 2_Multi Upload File

---
+++ JS08_PHPFormUpload/form_multiupload.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Multiuload Document</title>
+    </head>
+    <body>
+        <h2>upload Document</h2>
+        <form action="upload_process.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="files[]" multiple="">
+        </form>
+    </body>
+</html>
