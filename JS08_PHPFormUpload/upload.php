63dce7866baca6b65c717a385f0f137ffd6b08a4
rankadian <dianthafafian@gmail.com>
2024-10-15 16:14:08 +0700

    PRACTICAL 1 JS08

--- JS08_PHPFormUpload/upload.php
+++ JS08_PHPFormUpload/upload.php
@@ -1,11 +1,11 @@
 <?php
 if (isset($_POST["submit"])) {
     $targetdir = "uploads/";
-    $targetfile = $targetdir .basename($_FILES["myfile"]["name"]);
+    $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
     $fileType = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
-    $allowedExtensions = array("txt","pdf", "doc", "docx");
-    $maxsize = 3*1024*1024;
-    if (in_array($fileType, $allowedExtensions)&& $_FILES["myfile"]["size"]<=$maxsize) {
+    $allowedExtensions = array("txt", "pdf", "doc", "docx");
+    $maxsize = 3 * 1024 * 1024;
+    if (in_array($fileType, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize) {
         if (move_uploaded_file($_FILES["myfile"]["tmp_name"],  $targetfile)) {
             echo "File successed upload";
         } else {
@@ -13,6 +13,5 @@ if (isset($_POST["submit"])) {
         }
     } else {
         echo "file is invalid or exceeds the specified capacity size";
-} 
+    }
 }
-?>
